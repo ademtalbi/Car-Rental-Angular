a18bf571a1068de6fcba5f36b57e1618f6e452ea
ademtalbi <talbiadem22@gmail.com>
2025-12-21 00:25:06 +0100

    Fix navigation for Login and Sign Up buttons by moving routes to app.routes.ts for standalone routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "register", component: SignupComponent },
+  { path: "login", component: LoginComponent }
+];
